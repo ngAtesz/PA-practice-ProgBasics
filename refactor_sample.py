834eaa9449b1b93f6f1d166c697f1b29ad628626
ngAtesz <attila.molnar@codecool.com>
2020-03-19 16:06:05 +0100

    refactor_sample Rfactor first stage with two printing method

--- refactor_sample.py
+++ refactor_sample.py
@@ -1,43 +1,36 @@
 import random as r
 from time import sleep
 
-# The question here is: Why do you need 2 versions with and without printing?
-# Printing is a different concern from generating numbers...
-def generate_numbers_with_printing():
-    list_of_numbers = []
-    num_frequency = 9
-    for number in range(num_frequency):
-        # Why the number generated between 1 and 20? 
-        list_of_numbers.append(r.randint(1, 20))
-        print(list_of_numbers)
-        # Why do you need delay?
+
+def print_numbers(number_list):
+    for index in range(len(number_list)):
+        print(number_list[:index+1])
         sleep(0.1)
-    return list_of_numbers
 
 
-def generate_numbers_without_printing():
+
+def generate_numbers(count):
     list_of_numbers = []
-    # what is 9? why excatly 9?
-    for number in range(9):
+    for _ in range(count):
         list_of_numbers.append(r.randint(1, 20))
     return list_of_numbers
 
 
-def ascending_nums(numbers):
-    # it is not clear what num_frequency means and why is it declared here?
-    num_frequency = 9
-    for i in range(num_frequency):
-        print(numbers[:num_frequency])
-        num_frequency = num_frequency - 1
+def print_numbers_backwards(numbers):
+    upper_limit = len(numbers)
+    for i in range(upper_limit):
+        print(numbers[:upper_limit])
+        upper_limit = upper_limit - 1
         sleep(0.1)
 
 
 def main():
     program_frequency = 2
     for i in range(program_frequency):
-        generate_numbers_with_printing()
-        numlist = generate_numbers_without_printing()
-        ascending_nums(numlist)
+        nums = generate_numbers(9)
+        print_numbers(nums)
+        numlist = generate_numbers(9)
+        print_numbers_backwards(numlist)
 
 
 if __name__ == "__main__":
